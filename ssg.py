ac76fb09394a00875ebd14d16185725d731295bc
shadowfyt <60409766+shadowfyt@users.noreply.github.com>
2021-04-13 10:20:58 -0500

    part 2

--- ssg.py
+++ ssg.py
@@ -1,10 +1,12 @@
 import typer
+import ssg.parsers
 from ssg.site import Site
 
 def main(source="content", dest="dist"):
     config = {
         "source":source,
-        "dest":dest
+        "dest":dest,
+        "parsers": [ssg.parsers.ResourceParser()]
     }
 
     Site(**config).build()
--- ssg/site.py
+++ ssg/site.py
@@ -1,9 +1,11 @@
 from os import mkdir
 from pathlib import Path
+from ssg.parsers import Parser
 
 class Site():
     
-    def __init__(self, source, dest) -> None:
+    def __init__(self, source, dest, parsers = None) -> None:
+        self.parsers = parsers
         self.source = Path(source)
         self.dest = Path(dest)
     
@@ -17,7 +19,19 @@ class Site():
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
             
 
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
 
     
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse()
+        else:
+            print(NotImplemented)
